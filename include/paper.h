edc2949f2943f32e6f51e1898d91114e1477f63e
James Benner <james.benner@gmail.com>
2017-04-19 12:05:17 -0400

    Refactored into "paper_is_subsequent_word" function.

--- include/paper.h
+++ include/paper.h
@@ -6,8 +6,15 @@
 bool paper_is_first_word(char *ptr)
 {
     // Lookback and verify we don't have a NUL pointer then check that we
-    // aren't currently on a space. This finds the first word.
+    // aren't currently on a space.
     return (*(ptr - 1) == '\0' && *ptr != KATA_SPACE);
+}
+
+bool paper_is_subsequent_word(char *ptr)
+{
+    // Lookback and see if the prior character was a space then check that
+    // we aren't currently on a space.
+    return (*(ptr - 1) == KATA_SPACE && *ptr != KATA_SPACE);
 }
 
 size_t paper_word_count(char *paper)
@@ -20,13 +27,7 @@ size_t paper_word_count(char *paper)
             break;
         }
 
-        if (paper_is_first_word(ptr)) {
-            count += 1;
-        }
-
-        // Lookback and see if the prior character was a space then check that
-        // we aren't currently on a space. This finds subsequent words.
-        if (*(ptr - 1) == KATA_SPACE && *ptr != KATA_SPACE) {
+        if (paper_is_first_word(ptr) || paper_is_subsequent_word(ptr)) {
             count += 1;
         }
     } while (*(++ptr));
